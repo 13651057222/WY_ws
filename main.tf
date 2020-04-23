c499624e7ec5697d63efa9852390362420093b6c
13651057222 <13651057222@163.com>
2020-04-23 11:30:16 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+variable "access_key" {
+  //type = string
+}
+
+variable "secret_key" {
+  //type = string
+}
+
+variable "region" {
+  //type = string
+}
+
+provider "alicloud" {
+  access_key = ${access_key}
+  secret_key = ${secret_key}
+  version    = "~> 1.76"
+  region     = ${region}
+}
+
+data "alicloud_regions" "current_region_ds" {
+}
+
+data "alicloud_instance_types" "c2g4" {
+  cpu_core_count = 2
+  memory_size    = 4
+}
+
+data "alicloud_images" "images_ds" {
+  owners     = "system"
+  name_regex = "^centos_7"
+  output_file = "./image.file"
+}
+
+output "first_image_id" {
+  value = data.alicloud_images.images_ds.images.2.id
+  description = "aaaa"
+}
+
+output "region" {
+  value = ${region}
+}
