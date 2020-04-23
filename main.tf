c859b023c3db22d26190e469e53e04dcc2d98da9
13651057222 <13651057222@163.com>
2020-04-23 12:09:33 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,16 +45,16 @@ resource "alicloud_instance" "wy" {
   password              = "Abcd!234"
   image_id              = "${data.alicloud_images.images_ds.images.2.id}"
   internet_charge_type  = "PayByTraffic"
-
+  resource_group_id     = "rg-aek2z7njhung2ka"
   instance_type        = "${data.alicloud_instance_types.c2g4.instance_types.0.id}"
   system_disk_category = "cloud_efficiency"
   security_groups      = ["${alicloud_security_group.default.id}"]
-  instance_name        = "web"
-  vswitch_id           = "vsw-abc12345"
+  instance_name        = "wy"
+  vswitch_id           = "vsw-2ze4ma7dap6bira0q1ozk"
 }
 
 resource "alicloud_security_group" "default" {
   name        = "default"
   description = "default"
-  vpc_id      = "vpc-bp110ctzj23qp97c4c8wq"
+  vpc_id      = "vpc-2zeme4cm4udat3oniyxrz"
 }
